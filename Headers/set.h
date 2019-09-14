4554244bb0038b07653c18c72b7e171311880bdb
dlvguo <dlvuo@outlook.com>
2019-09-14 11:01:11 +0800

    Update set.h

--- Headers/set.h
+++ Headers/set.h
@@ -121,7 +121,8 @@ int ArrayIndex(int x){
     return x>>4;
 }
 
-//计算这个数在一个ushort里面所在的第几个位置 除16取余即可，之后向右移变能算出位置
+//计算这个数在一个ushort里面所在的第几个位置 除16取余+1即可
+//输入10二进制为1010为15 1111 &计算得1010表示10 之后右移
 unsigned short BitMask(int x){
     return 1<<(x&15);
 }
@@ -131,7 +132,10 @@ Set InitSet(int size)
 {
     Set _set = new Bitset;
     _set->size = size;
+    //一个US类型可以表示16个 故数组大小为1+size/16
+    //书中这种向右移动4位的运算方式等同于/16
     _set->arraysize = (size+15)>>4;
+    //书中v大小为size 个人认为有错,有不同意见的朋友欢迎探讨
     _set->v = new unsigned short[_set->arraysize];
     for (int i = 0; i < size; i++)
     {
@@ -140,14 +144,14 @@ Set InitSet(int size)
     return _set;
 }
 
-//把a赋值给b
-void SetAsign(Set a, Set b)
+//把B赋值给A
+void SetAsign(Set A, Set B)
 {
-    if (a->size != b->size)
+    if (A->size != B->size)
         return;
-    for (int i = 0; i < a->size; i++)
+    for (int i = 0; i < A->size; i++)
     {
-        b->v [i] = a->v[i];
+        A->v [i] = B->v[i];
     }
 }
 
@@ -156,61 +160,63 @@ int SetMember(int x,Set set){
     if(x<0||x>set->size)
         return 0;
     //首先查找数组对应位置，与x相对的位置
-    //如3 此时在数组第一个位置 BitMask值为1000 
+    //如3 此时在数组第一个位置 BitMask值为1000 表示第4数，第1个为0
     return set->v[ArrayIndex(x)]&BitMask(x);
 }
 
 //判断集合是否相等一一判断
-int Equal(Set s1,Set s2){
-    if(s1->size!=s2->size)
+int SetEqual(Set A,Set B){
+    if(A->size! B->size)
         return 0;
-    for (int i = 0; i < s1->arraysize; i++)
+    for (int i = 0; i < A->arraysize; i++)
     {
-        if(s1->v[i]!=s2->v[i])
+        if(A->v[i]! B->v[i])
             return 0;
     }
     return 1;
 }
 
-//取并集  如s1=1100 s2=0100 此时第三个为并集
-Set SetUnion(Set s1,Set s2){
-    Set s=InitSet(s1->size);
+//取并集  如A=1100 B=0110=>1110 此时第2、3、4个为并集
+Set SetUnion(Set A,Set B){
+    Set s=InitSet(A->size);
     for (int i = 0; i < s->arraysize; i++)
     {
         //按位|
-        s->v[i]=s1->v[i]|s2->v[i];
+        s->v[i]=A->v[i] B->v[i];
     }
 }
 
 
-//取交集  如s1=1100 s2=0100 此时第三个为并集
-Set SetIntersection(Set s1,Set s2){
-    Set s=InitSet(s1->size);
+//取交集  如A=1100 B=0100=>0100 此时第3个为交集
+Set SetIntersection(Set A,Set B){
+    Set s=InitSet(A->size);
     for (int i = 0; i < s->arraysize; i++)
     {
         //按位&
-        s->v[i]=s1->v[i]&s2->v[i];
+        s->v[i]=A->v[i] B->v[i];
     }
 }
 
-//取差集  如s1=1100 s2=0100 此时第三个为并集
-Set SetDifference(Set s1,Set s2){
-    Set s=InitSet(s1->size);
+//取差集  如A=111000 B=001000=>取&为001000 再异或110000 此时第5.6个为差集
+Set SetDifference(Set A,Set B){
+    Set s=InitSet(A->size);
     for (int i = 0; i < s->arraysize; i++)
     {
         //按位& 再^
-        s->v[i]=s1->v[i]^(s1->v[i]&s2->v[i]);
+        s->v[i]=A->v[i]^(A->v[i] B->v[i]);
     }
 }
 
 //位插入运算 查找数组位置  找到x%16放置 用|运算
+//如17插入，此时数组v[1]中为00100000，表示第七个位置有东西，17%16+1=2 |运算后v[1]为00100010
 void SetInsert(int x,Set s){
     if(x<0||s->size<x)
         return;
     s->v[ArrayIndex(x)]|=BitMask(x);
 }
 
-//删除元素运算 先取反再去取
+//删除元素运算 先取反再去取&
+//如删除17，此时数组v[1]中为00100010，表示第2、7个位置有东西，17%16+1=2 ~2取反为11111101 &运算后v[1]为00100000
 void SetDelete(int x,Set s ){
      if(x<0||s->size<x)
         return;
