a75dbcdf5d3de757717426c8782ef37be385a0f2
dlvguo <dlvuo@outlook.com>
2019-10-16 22:54:59 +0800

    Update P157-19.cpp

--- BaiPiShu/No.8Select/P157-19.cpp
+++ BaiPiShu/No.8Select/P157-19.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include"../../Headers/BinaryTree.h"
+#include<math.h>
 using namespace std;
 
-bool JudgeBalance(BiTree a,BiTree b){
-    //ab不相等
-    if(a!=b)
-        return false;
-    //ab为空
-    if(a==NULL)
-        return true;
-    return JudgeBalance(a->lchild,b->lchild)&&JudgeBalance(a->rchild,b->rchild);
+//判断
+int JudgeBalance(BiTree bt){
+    if(bt==NULL)
+        return 0;
+    //平衡因子等于左子树-右子树高度
+    int hl=JudgeBalance(bt->lchild),hr=JudgeBalance(bt->rchild);
+    //返回-1表示错误不平衡
+    if(hl==-1||hr==-1||abs(hr-hl)>1)
+        return -1;
+    //返回本层高度
+    return hl>hr?1+hl:1+hr;
 }
