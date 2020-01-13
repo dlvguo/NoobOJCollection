949a1c0368f9ff47d77d538334361f8128f19202
ChenJG <dlvguo@qq.com>
2020-01-13 12:24:40 +0800

    Update 10.cpp

--- LeetCode/10.cpp
+++ LeetCode/10.cpp
@@ -2,50 +2,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool FirstMatch(char a,char b){
+    return a==b||b=='.';
+}
+
 bool isMatch(char *s, char *p)
 {
-    int slen = strlen(s), plen = strlen(p);
-    if(plen==0)
-        return slen==0?true:false;
-    if (slen==0){
-        if(plen==1||p[1]!='*')
-            return false;
-         return isMatch(s,p+2);
+    //匹配式为0的话
+    if(*p==0)
+        return *s==0;
+    if(*(p+1)=='*'){
+        return isMatch(s,p+2)||(*s&&FirstMatch(*s,*p)&&isMatch(s+1,p));
     }
-    if (p[0] == '.')
-    {
-        if (plen == 1 || p[1] != '*')
-            return isMatch(s + 1, p + 1);
-        if(plen==2)
-            return true;
-        for (int i = slen; i>=0; i++)
-        {
-            if(isMatch( s+i,p+2))
-                return true;
-        }
-        return false;
-    }
-    //俩种情况 一种是匹配的一种是*的
-    if(plen>=2&&p[1]=='*'){
-        int count=0,tempp=plen+2,sindex=0;    
-        while (tempp<plen&&sindex<slen&&s[sindex]==p[tempp])
-        {
-            count++;
-        }
-        for (int i = count; i>=0; i++)
-        {
-            if(isMatch( s+i,p+2))
-                return true;
-        }
-        return false;
-    }
-    else if(s[0]==p[0])
-        return isMatch(s+1,p+1);
-    return false;
+    return *s&&FirstMatch(*s,*p)&&isMatch(s+1,p+1);
 }
 
 int main()
 {
-    char *s ="mississippi", *p ="mis*is*p*.";
-    cout << isMatch(s, p);
+    char *s ="mississippi", *p ="mis*is*p*.";//空就为0
+    cout<<isMatch(s,p);
 }
