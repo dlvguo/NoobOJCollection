5a7628ce878e0bd70e69c8f0b3ca7bbe5bfbf17a
ChenJG <dlvguo@qq.com>
2020-01-17 19:02:28 +0800

    Create 22.cpp

---
+++ LeetCode/22.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+
+void dfs(vector<string> ans,)
+
+
+vector<string> generateParenthesis(int n)
+{
+    vector<string> ans;
+    if(!n)
+        return;
+    
+}
