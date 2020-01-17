f814d7f69f5af890524fd0783bb8ae14bc14bf27
dlvguo <dlvuo@outlook.com>
2020-01-17 21:40:00 +0800

    Update 22.cpp

--- LeetCode/22.cpp
+++ LeetCode/22.cpp
@@ -5,13 +5,22 @@
 using namespace std;
 
 
-void dfs(vector<string> ans,)
+void dfs(vector<string> &ans,string res,int l,int r,int n){
+    if(r>l||l>n||r>n)
+        return;
+    if(l==r&&r==n){
+        ans.push_back(res);
+    }
+    dfs(ans,res+'(',l+1,r,n);
+    dfs(ans,res+')',l,r+1,n);
+}
 
 
 vector<string> generateParenthesis(int n)
 {
     vector<string> ans;
     if(!n)
-        return;
-    
+        return ans;
+    dfs(ans,"",0,0,n);
+    return ans;
 }
