c5a3b49ac2b102e6d42dafa8bfa98933a605ceb7
ChenJG <dlvguo@qq.com>
2020-02-23 12:25:05 +0800

    Create 03.cpp

---
+++ LeetCode/JianZhiOffer/03.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include<cstring>
+using namespace std;
+int findRepeatNumber(vector<int> &nums)
+{
+    int *n=new int[nums.size()];
+    memset(n,0,sizeof(n));
+    for (int i = 0; i <nums.size(); i++)
+    {
+        if(n[nums[i]]==0)
+            n[nums[i]]=1;
+        else{
+            return nums[i];
+        }
+    }
+    return 0;
+    
+}
+
+int main()
+{
+        int *n=new int[5];
+            memset(n,0,sizeof(int)*5);
+        cout<<n[4];
+        system("pause");
+}
