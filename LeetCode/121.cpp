6548148e00dab7313f82f2d1b3c7be64cef2aca7
ChenJG <dlvguo@qq.com>
2020-03-09 13:41:40 +0800

    Update 121.cpp

--- LeetCode/121.cpp
+++ LeetCode/121.cpp
@@ -27,8 +27,33 @@ public:
     }
 };
 
-int mian(){
+//动态规划法
+int maxProfit(vector<int> &prices)
+{
+    if (prices.size() <= 1)
+        return 0;
+    int n = prices[0];
+    int max = 0;
+    for (int i = 1; i < prices.size(); i++)
+    {
+        if (prices[i] > n)
+        {
+            max = max > prices[i] - n ? max : prices[i] - n;
+        }
+        else
+        {
+            max = prices[i];
+        }
+    }
+
+    return max;
+}
+
+int main()
+{
     Solution s1;
-    vector<int> v={7,6,4,3,1};
-    cout<<s1.maxProfit(v);
+    vector<int> v = {7, 6, 4, 3, 1};
+    cout << s1.maxProfit(v);
+    system("pause");
+    return 0;
 }
