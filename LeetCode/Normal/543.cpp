7d1a97220785b2d9faf3291bd178f4f1c19ad207
ChenJG <dlvguo@qq.com>
2020-03-10 22:40:45 +0800

    Create 543.cpp

---
+++ LeetCode/Normal/543.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+public:
+    int result = 0;
+
+    int dfs(TreeNode *root)
+    {
+        if (!root)
+            return 0;
+        int left = diameterOfBinaryTree(root->left);
+        int right = diameterOfBinaryTree(root->right);
+        if (result < left + right)
+            result = left + right;
+        return left > right ? left + 1 : right + 1;
+    }
+
+    int diameterOfBinaryTree(TreeNode *root)
+    {
+        dfs(root);
+        return result;
+    }
+};
