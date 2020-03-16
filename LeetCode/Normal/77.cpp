309bb4780170ace7125509bea9c6e17c7aa09ced
ChenJG <dlvguo@qq.com>
2020-03-16 19:10:05 +0800

    Update 77.cpp

--- LeetCode/Normal/77.cpp
+++ LeetCode/Normal/77.cpp
@@ -5,27 +5,6 @@ class Solution
 {
 public:
     vector<vector<int>> vts;
-    vector<bool> visit;
-    void dfs(vector<int> nums, vector<int> &pointNums, int steps = 0)
-    {
-        if (steps == nums.size())
-        {
-            vts.push_back(pointNums);
-        }
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (visit[i])
-                continue;
-            else
-            {
-                visit[i] = true;
-                pointNums.push_back(nums[i]);
-                dfs(nums, pointNums, steps + 1);
-                visit[i] = false;
-                pointNums.pop_back();
-            }
-        }
-    }
 
     void dfsPutNums(int n, int nowNum, int k, vector<int> &nums)
     {
@@ -36,8 +15,7 @@ public:
         nums.push_back(nowNum);
         if (nums.size() == k)
         {
-            vector<int> pointNums;
-            dfs(nums, pointNums);
+            vts.push_back(nums);
         }
         else
             dfsPutNums(n, nowNum + 1, k, nums);
@@ -49,7 +27,6 @@ public:
     {
         if (!n)
             return {};
-        visit.resize(k, false);
         vector<int> nums;
         dfsPutNums(n, 1, k, nums);
         return vts;
