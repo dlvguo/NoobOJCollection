0d2afd83399e1510ab1131ab4859e02ff8ed9262
ChenJG <dlvguo@qq.com>
2020-03-28 17:33:26 +0800

    Create 55-1.cpp

---
+++ LeetCode/JianZhiOffer/55-1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+public:
+    int maxDepth(TreeNode *root)
+    {
+        if (!root)
+            return 0;
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
