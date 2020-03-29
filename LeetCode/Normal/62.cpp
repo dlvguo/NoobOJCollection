4a7dd4e107384e864518151576003395bf693284
ChenJG <dlvguo@qq.com>
2020-03-30 00:21:21 +0800

    Create 62.cpp

---
+++ LeetCode/Normal/62.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    int lastRemaining(int n, int m)
+    {
+        int p = 0;
+        for (int i = 2; i <= n; i++)
+        {
+            p = (p + m) % i;
+        }
+        return p;
+    }
+};
+int main() {}
