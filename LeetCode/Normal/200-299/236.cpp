9fd599a596c8f577ba347d6b9993a0e53e49c7c4
ChenJG <dlvguo@qq.com>
2020-04-06 00:12:58 +0800

    Create 236.cpp

---
+++ LeetCode/Normal/200-299/236.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        if (root == NULL || root == p || root == q)
+            return root;
+        TreeNode *l = lowestCommonAncestor(root->left, p, q);
+        TreeNode *r = lowestCommonAncestor(root->right, p, q);
+        if (l && r)
+            return root;
+        return l ? l : r;
+    }
+};
