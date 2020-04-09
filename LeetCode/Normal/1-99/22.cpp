f03e7ecea3f6eff64849ff9a58de239a0c7715cc
ChenJG <dlvguo@qq.com>
2020-04-09 16:46:40 +0800

    Update 22.cpp

--- LeetCode/Normal/1-99/22.cpp
+++ LeetCode/Normal/1-99/22.cpp
@@ -4,23 +4,27 @@
 #include <vector>
 using namespace std;
 
-
-void dfs(vector<string> &ans,string res,int l,int r,int n){
-    if(r>l||l>n||r>n)
+void dfs(vector<string> &ans, string &res, int l, int r, int n)
+{
+    if (r > l || l > n || r > n)
         return;
-    if(l==r&&r==n){
+    if (l == r && r == n)
+    {
         ans.push_back(res);
     }
-    dfs(ans,res+'(',l+1,r,n);
-    dfs(ans,res+')',l,r+1,n);
+    res.push_back('(');
+    dfs(ans, res, l + 1, r, n);
+    res.pop_back();
+    res.push_back(')');
+    dfs(ans, res, l, r + 1, n);
 }
-
 
 vector<string> generateParenthesis(int n)
 {
     vector<string> ans;
-    if(!n)
+    if (!n)
         return ans;
-    dfs(ans,"",0,0,n);
+    string str;
+    dfs(ans, str, 0, 0, n);
     return ans;
 }
