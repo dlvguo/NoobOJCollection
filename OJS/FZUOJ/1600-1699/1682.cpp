e37a431050370171dd2db596eab458d357abc25a
ChenJG <dlvguo@qq.com>
2020-04-14 10:18:38 +0800

    Update 1682.cpp

--- OJS/FZUOJ/1600-1699/1682.cpp
+++ OJS/FZUOJ/1600-1699/1682.cpp
@@ -3,114 +3,45 @@
 using namespace std;
 typedef long long LL;
 
-// int main()
-// {
-// 	//改下了 可能爆了
-// 	LL num = 10, table[25] = {1, 8};
-// 	for (int i = 2; i < 20; i++)
-// 	{
-// 		table[i] = table[i - 1] * 8;
-// 		num *= 10;
-// 	}
-// 	////可能是有超界的问题
-// 	//for (int j = 19; j >= 1; j--)
-// 	//{
-// 	//	table[j] = num - table[j];
-// 	//	num /= 10;
-// 	//}
-// 	string str;
-// 	while (cin >> str)
-// 	{
-// 		LL sum = 0;
-// 		//num = pow(10, str.size() - 1);
-// 		for (int i = 0, len = str.size() - 1; i < str.size(); i++, len--)
-// 		{
-
-// 			int n = str[i] - '0'; //获取数字
-// 			if (n)
-// 			{
-// 				if (n > 5)
-// 				{
-// 					n -= 2;
-// 				}
-// 				else if (n > 2)
-// 				{
-// 					n -= 1;
-// 				}
-// 				sum += table[len] * n;
-// 			}
-// 			//num /= 10;
-// 		}
-// 		cout << sum << endl;
-// 	}
-// }
-
-#include <stdio.h>
-
-#include <string.h>
-
-#include <stdlib.h>
-
-#include <math.h>
-
-#define le 60
-
-long long n;
-
-long long d[le];
-
-char s[le];
-
-int main(void)
+int main()
 {
-
-	int k, i, m;
-
-	long long ans;
-
-	while (scanf("%s", &s) != EOF)
+	//打表下计算下每位数有多少个正常数字 比如10有8个 100内有64个 1000内有512个 因为2^64次方<10^20 因此计算19个即可
+	LL table[25] = {1, 8};
+	for (int i = 2; i < 20; i++)
 	{
-
-		ans = k = 0;
-
-		m = (int)strlen(s);
-
-		for (i = m - 1; i >= 0; i--)
-
-			d[k++] = s[i] - '0';
-
-		for (i = k - 1; i >= 0; i--)
-		{
-
-			if (d[i] <= 3)
-			{
-
-				ans += d[i] * (long long)pow(8., i);
-
-				if (d[i] == 3)
-				{
-
-					ans--;
-
-					break;
-				}
-			}
-
-			else if (d[i] >= 4 && d[i] <= 6)
-			{
-				ans += (d[i] - 1) * (long long)pow(8., i);
-				if (d[i] == 6)
-				{
-					ans--;
-					break;
-				}
-			}
-			else
-				ans += (d[i] - 2) * (long long)pow(8., i);
-		}
-
-		printf("%lld\n", ans);
+		table[i] = table[i - 1] * 8;
 	}
-
-	return 0;
+	string str;
+	while (cin >> str)
+	{
+		LL sum = 0;
+		//从最大数计算 如22312=2*10000（8^4） +2312
+		for (int i = 0, len = str.size() - 1; i < str.size(); i++, len--)
+		{
+			int n = str[i] - '0'; //获取数字
+			int t = n;
+			//n不为0的情况
+			if (n)
+			{
+				//去除包含3和6的数
+				if (n > 6)
+				{
+					n -= 2;
+				}
+				else if (n > 3)
+				{
+					n -= 1;
+				}
+				sum += table[len] * n;
+				//如果此时数字为6或者3的话需要跳出 如6532=6000+532 因为最大位为6 剩下的后面数字无需计算
+				if (t == 6 || t == 3)
+				{
+					sum--;
+					break;
+				}
+			}
+		}
+		cout << sum << endl;
+	}
 }
+
