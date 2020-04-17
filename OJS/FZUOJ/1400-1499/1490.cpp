8f98337c17d2c86a29b6c602b80f0e1249c1296f
ChenJG <dlvguo@qq.com>
2020-04-17 20:06:07 +0800

    up

--- OJS/FZUOJ/1400-1499/1490.cpp
+++ OJS/FZUOJ/1400-1499/1490.cpp
@@ -1,2 +1,30 @@
 #include <iostream>
 using namespace std;
+
+int main()
+{
+    string str;
+    int sum = 0;
+    while (getline(cin, str))
+    {
+        for (int i = 0; i < str.size(); i++)
+        {
+            if (str[i] == 'E' && str[i + 1] == 'N')
+            {
+                if (sum % 2)
+                {
+                    cout << 'B';
+                }
+                else
+                {
+                    cout << 'W';
+                }
+                cout << ' ' << sum << endl;
+                sum = 0;
+                break;
+            }
+            else if (str[i] >= '0' && str[i] <= '9' && str[i - 1] >= 'A')
+                sum++;
+        }
+    }
+}
