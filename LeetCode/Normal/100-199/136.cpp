f3329c90c11023b1339fadf3715a2efa13b25ddd
ChenJG <dlvguo@qq.com>
2020-05-14 00:15:00 +0800

    打卡

---
+++ LeetCode/Normal/100-199/136.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        int num = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            num ^= nums[i];
+        }
+        return num;
+    }
+};
