0e14dbd0df20f847e243eabb463459d77d16cbd3
ChenJG <dlvguo@qq.com>
2020-05-15 22:16:25 +0800

    Create 560.cpp

---
+++ LeetCode/Normal/500-599/560.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int subarraySum(vector<int> &nums, int k)
+    {
+         int count = 0;
+        for (int start = 0; start < nums.size(); ++start) {
+            int sum = 0;
+            for (int end = start; end >= 0; --end) {
+                sum += nums[end];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
