6f9a415a3105efeee58eacd4e5c9d717c7bebff6
ChenJG <dlvguo@qq.com>
2020-06-01 14:59:21 +0800

    Create 1431.cpp

---
+++ LeetCode/Normal/1400-1499/1431.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<bool> kidsWithCandies(vector<int> &candies, int extraCandies)
+    {
+        int n = candies.size();
+        int maxCandies = *max_element(candies.begin(), candies.end());
+        vector<bool> ret;
+        for (int i = 0; i < n; ++i)
+        {
+            ret.push_back(candies[i] + extraCandies >= maxCandies);
+        }
+        return ret;
+    }
+};
