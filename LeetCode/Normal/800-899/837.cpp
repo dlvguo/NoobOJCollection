1c99083dcb5356c988048d1618761f248e2f08f4
ChenJG <dlvguo@qq.com>
2020-06-03 20:42:10 +0800

    Create 837.cpp

---
+++ LeetCode/Normal/800-899/837.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    double new21Game(int N, int K, int W) {
+        double num1=0;
+         if (K == 0)
+            return 1.0;
+        vector<double> dp(K+W);
+        for(int i=K;i<=N &&i<K+W;i++)
+        {
+            dp[i]=1.0;
+            num1+=dp[i];
+        }
+        for(int i=K-1;i>=0;i--)
+        {
+            dp[i]+=num1/W;
+            num1=num1-dp[i+W]+dp[i];
+        }
+        return dp[0];
+    }
+};
