2c1d30bb29ee4ad2335e3e9a6ebaf711cc35e223
ChenJG <dlvguo@qq.com>
2020-07-03 19:22:14 +0800

    up

---
+++ LeetCode/Normal/100-199/108.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        def helper(left, right):
+            if left > right:
+                return None
+
+            # 总是选择中间位置右边的数字作为根节点
+            mid = (left + right + 1) // 2
+
+            root = TreeNode(nums[mid])
+            root.left = helper(left, mid - 1)
+            root.right = helper(mid + 1, right)
+            return root
+
+        return helper(0, len(nums) - 1)
---
+++ LeetCode/Normal/300-399/378.py
@@ -0,0 +1,4 @@
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        rec = sorted(sum(matrix, []))
+        return rec[k - 1]
