abd223728a2000b1f4496cd430d4ca5a9639445b
ChenJG <dlvguo@qq.com>
2020-07-04 20:58:53 +0800

    Create 32.py

---
+++ LeetCode/Normal/1-99/32.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        if not s: return 0
+
+        stack = []
+        ans = 0
+        for i in range(len(s)):
+            # 入栈条件
+            if not stack or s[i] == '(' or s[stack[-1]] == ')':
+                stack.append(i)     # 下标入栈
+            else:
+                # 计算结果
+                stack.pop()
+                ans = max(ans, i - (stack[-1] if stack else -1))
+        return ans
+
