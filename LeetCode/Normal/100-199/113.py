1d9bb4cddaa362cc08242504748e398558939560
ChenJG <dlvguo@qq.com>
2020-09-26 08:52:14 +0800

    up

--- LeetCode/Normal/100-199/113.py
+++ LeetCode/Normal/100-199/113.py
@@ -1,11 +1,32 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val = 0, neighbors = []):
-        self.val = val
-        self.neighbors = neighbors
-"""
+# Definition for a binary tree node.
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 
 class Solution:
-    def cloneGraph(self, node: 'Node') -> 'Node':
-        return copy.deepcopy(node)
+    def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
+        self.paths = []
+        self.temp = []
+
+        def dfs(ans: int, root: TreeNode):
+            if not root:
+                return
+            ans += root.val
+            self.temp.append(root.val)
+            if ans == sum:
+                if not root.right and not root.left:
+                    self.paths.append(self.temp[:])
+                    self.temp.pop()
+                    return
+            dfs(ans, root.left)
+            dfs(ans, root.right)
+            self.temp.pop()
+
+        dfs(0, root)
+        return self.paths
