0659782c8fbb848cec5a6bbde40f9bb4e532230b
ChenJG <dlvguo@qq.com>
2020-10-17 09:27:32 +0800

    Update 52.cpp

--- LeetCode/Normal/1-99/52.cpp
+++ LeetCode/Normal/1-99/52.cpp
@@ -4,14 +4,17 @@ using namespace std;
 /*N皇后问题有个计算优化思路
   左对角线 row-lie=n 这个n对于同个对角线的值相同 
   右对角线 row+lie=n 这个n对于同个对角线的值相同 
-  正对角线用数组保存即可
+  正对角线用数组列下标映射即可
 */
 class Solution
 {
 public:
     int ans = 0;
+    //正对角线
     int visit[60] = {0};
+    //左对角线
     int visit2[60] = {0};
+    //右对角线
     int visit3[60] = {0};
     int totalNQueens(int n)
     {
@@ -27,6 +30,7 @@ public:
         }
         for (int i = 0; i < n; i++)
         {
+            //i-row+n 避免负数
             if (visit[i] || visit2[i - row + n] || visit3[i + row])
             {
                 continue;
