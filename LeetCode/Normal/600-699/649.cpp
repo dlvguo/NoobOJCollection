ac5d0fbfaf282073355eb67e8144c7703a11e106
ChenJG <dlvguo@qq.com>
2020-12-11 09:42:37 +0800

    Create 649.cpp

---
+++ LeetCode/Normal/600-699/649.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    string predictPartyVictory(string senate)
+    {
+        int n = senate.size();
+        queue<int> radiant, dire;
+        for (int i = 0; i < n; ++i)
+        {
+            if (senate[i] == 'R')
+            {
+                radiant.push(i);
+            }
+            else
+            {
+                dire.push(i);
+            }
+        }
+        while (!radiant.empty() && !dire.empty())
+        {
+            if (radiant.front() < dire.front())
+            {
+                radiant.push(radiant.front() + n);
+            }
+            else
+            {
+                dire.push(dire.front() + n);
+            }
+            radiant.pop();
+            dire.pop();
+        }
+        return !radiant.empty() ? "Radiant" : "Dire";
+    }
+};
