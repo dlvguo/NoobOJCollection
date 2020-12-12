e1d0c3af5b9e35c3084b27d9b27e0798c797081e
ChenJG <dlvguo@qq.com>
2020-12-12 09:36:53 +0800

    Update 376.cpp

--- LeetCode/Normal/300-399/376.cpp
+++ LeetCode/Normal/300-399/376.cpp
@@ -3,6 +3,8 @@ using namespace std;
 class Solution
 {
 public:
+    // 维护一个数组 up序列和down序列
+    // 只需要俩个数即可
     int wiggleMaxLength(vector<int> &nums)
     {
         int n = nums.size();
@@ -10,26 +12,20 @@ public:
         {
             return n;
         }
-        vector<int> up(n), down(n);
-        up[0] = down[0] = 1;
+        int u = 1, d = 1;
         for (int i = 1; i < n; i++)
         {
+            // 升序的序列 
             if (nums[i] > nums[i - 1])
             {
-                up[i] = max(up[i - 1], down[i - 1] + 1);
-                down[i] = down[i - 1];
+                u = max(u, d + 1);
             }
+            // 降序
             else if (nums[i] < nums[i - 1])
             {
-                up[i] = up[i - 1];
-                down[i] = max(up[i - 1] + 1, down[i - 1]);
-            }
-            else
-            {
-                up[i] = up[i - 1];
-                down[i] = down[i - 1];
+                d = max(u + 1, d);
             }
         }
-        return max(up[n - 1], down[n - 1]);
+        return max(u, d);
     }
 };
