e81c689f1ef5f7a956740a3b56da075428e61c15
ChenJG <dlvguo@qq.com>
2020-12-19 13:16:19 +0800

    Create 48.cpp

---
+++ LeetCode/Normal/1-99/48.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    void rotate(vector<vector<int>> &matrix)
+    {
+        int n = matrix.size();
+        // 水平翻转
+        for (int i = 0; i < n / 2; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                swap(matrix[i][j], matrix[n - i - 1][j]);
+            }
+        }
+        // 主对角线翻转
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < i; ++j)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+    }
+};
