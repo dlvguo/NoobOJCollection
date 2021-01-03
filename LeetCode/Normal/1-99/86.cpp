279521842b91e34181de66751d457a39594ffed0
ChenJG <dlvguo@qq.com>
2021-01-03 13:32:20 +0800

    Create 86.cpp

---
+++ LeetCode/Normal/1-99/86.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    ListNode *partition(ListNode *head, int x)
+    {
+        ListNode *small = new ListNode(0);
+        ListNode *smallHead = small;
+        ListNode *large = new ListNode(0);
+        ListNode *largeHead = large;
+        while (head != nullptr)
+        {
+            if (head->val < x)
+            {
+                small->next = head;
+                small = small->next;
+            }
+            else
+            {
+                large->next = head;
+                large = large->next;
+            }
+            head = head->next;
+        }
+        large->next = nullptr;
+        small->next = largeHead->next;
+        return smallHead->next;
+    }
+};
