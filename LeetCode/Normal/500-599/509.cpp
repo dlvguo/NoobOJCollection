8cfe49b76e12c4bccc8662669b507c5cc692452b
ChenJG <dlvguo@qq.com>
2021-01-04 08:52:11 +0800

    Create 509.cpp

---
+++ LeetCode/Normal/500-599/509.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int fib(int n)
+    {
+        if (n <= 1)
+            return n;
+        return fib(n - 1) + fib(n - 2);
+    }
+};
