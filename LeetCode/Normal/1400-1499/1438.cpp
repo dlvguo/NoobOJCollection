d943e2b7158a2fdc28fc7028360410af6abe684a
ChenJG <dlvguo@qq.com>
2021-02-22 04:08:52 +0800

    up

---
+++ LeetCode/Normal/1400-1499/1438.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int longestSubarray(vector<int> &nums, int limit)
+    {
+        multiset<int> s;
+        int n = nums.size();
+        int left = 0, right = 0;
+        int ret = 0;
+        while (right < n)
+        {
+            s.insert(nums[right]);
+            while (*s.rbegin() - *s.begin() > limit)
+            {
+                s.erase(s.find(nums[left++]));
+            }
+            ret = max(ret, right - left + 1);
+            right++;
+        }
+        return ret;
+    }
+};
