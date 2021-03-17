f62d16445ba890fb7e70b89a4d5fd262ff21e6fe
ChenJG <dlvguo@qq.com>
2021-03-17 19:49:50 +0800

    Create 115.cpp

---
+++ LeetCode/Normal/100-199/115.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int numDistinct(string s, string t)
+    {
+        int m = s.length(), n = t.length();
+        if (m < n)
+        {
+            return 0;
+        }
+        vector<vector<long>> dp(m + 1, vector<long>(n + 1));
+        for (int i = 0; i <= m; i++)
+        {
+            dp[i][n] = 1;
+        }
+        for (int i = m - 1; i >= 0; i--)
+        {
+            char sChar = s.at(i);
+            for (int j = n - 1; j >= 0; j--)
+            {
+                char tChar = t.at(j);
+                if (sChar == tChar)
+                {
+                    dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j];
+                }
+                else
+                {
+                    dp[i][j] = dp[i + 1][j];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+};
