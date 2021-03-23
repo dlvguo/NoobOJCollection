a6533322587f7f927b9723bb107e441bb2f3e63f
ChenJG <dlvguo@qq.com>
2021-03-23 10:41:08 +0800

    Create 341.cpp

---
+++ LeetCode/Normal/300-399/341.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class NestedInteger
+{
+public:
+    // Return true if this NestedInteger holds a single integer, rather than a nested list.
+    bool isInteger() const;
+
+    // Return the single integer that this NestedInteger holds, if it holds a single integer
+    // The result is undefined if this NestedInteger holds a nested list
+    int getInteger() const;
+
+    // Return the nested list that this NestedInteger holds, if it holds a nested list
+    // The result is undefined if this NestedInteger holds a single integer
+    const vector<NestedInteger> &getList() const;
+};
+
+class NestedIterator
+{
+private:
+    vector<int> vals;
+    vector<int>::iterator cur;
+
+    void dfs(const vector<NestedInteger> &nestedList)
+    {
+        for (auto &nest : nestedList)
+        {
+            if (nest.isInteger())
+            {
+                vals.push_back(nest.getInteger());
+            }
+            else
+            {
+                dfs(nest.getList());
+            }
+        }
+    }
+
+public:
+    NestedIterator(vector<NestedInteger> &nestedList)
+    {
+        dfs(nestedList);
+        cur = vals.begin();
+    }
+
+    int next()
+    {
+        return *cur++;
+    }
+
+    bool hasNext()
+    {
+        return cur != vals.end();
+    }
+};
