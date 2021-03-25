80d05cd6aa6429b6cb45ad9d8fab2062da040a92
ChenJG <dlvguo@qq.com>
2021-03-25 13:01:15 +0800

    Create 82.cpp

---
+++ LeetCode/Normal/1-99/82.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *deleteDuplicates(ListNode *head)
+    {
+        if (!head)
+        {
+            return head;
+        }
+
+        ListNode *dummy = new ListNode(0, head);
+
+        ListNode *cur = dummy;
+        while (cur->next && cur->next->next)
+        {
+            if (cur->next->val == cur->next->next->val)
+            {
+                int x = cur->next->val;
+                while (cur->next && cur->next->val == x)
+                {
+                    cur->next = cur->next->next;
+                }
+            }
+            else
+            {
+                cur = cur->next;
+            }
+        }
+
+        return dummy->next;
+    }
+};
