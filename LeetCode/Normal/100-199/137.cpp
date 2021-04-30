19f95831a585fdc85397490a3d325aec64b45c39
ChenJG <dlvguo@qq.com>
2021-04-30 20:08:43 +0800

    Create 137.cpp

---
+++ LeetCode/Normal/100-199/137.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        unordered_map<int, int> freq;
+        for (int num : nums)
+        {
+            ++freq[num];
+        }
+        int ans = 0;
+        for (auto [num, occ] : freq)
+        {
+            if (occ == 1)
+            {
+                ans = num;
+                break;
+            }
+        }
+        return ans;
+    }
+};
