5d2bf74ef6d73a71fef4b02f52801342092d4440
ChenJG <dlvguo@qq.com>
2021-05-25 21:37:35 +0800

    up

---
+++ LeetCode/Normal/600-699/664.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int strangePrinter(string s)
+    {
+        int n = s.length();
+        vector<vector<int>> f(n, vector<int>(n));
+        for (int i = n - 1; i >= 0; i--)
+        {
+            f[i][i] = 1;
+            for (int j = i + 1; j < n; j++)
+            {
+                if (s[i] == s[j])
+                {
+                    f[i][j] = f[i][j - 1];
+                }
+                else
+                {
+                    int minn = INT_MAX;
+                    for (int k = i; k < j; k++)
+                    {
+                        minn = min(minn, f[i][k] + f[k + 1][j]);
+                    }
+                    f[i][j] = minn;
+                }
+            }
+        }
+        return f[0][n - 1];
+    }
+};
