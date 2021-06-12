e252954c6ac1a5b238c5a29faced2b9187291dac
ChenJG <dlvguo@qq.com>
2021-06-12 17:44:21 +0800

    Create 279.cpp

---
+++ LeetCode/Normal/200-299/279.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int numSquares(int n)
+    {
+        vector<int> f(n + 1);
+        for (int i = 1; i <= n; i++)
+        {
+            int minn = INT_MAX;
+            for (int j = 1; j * j <= i; j++)
+            {
+                minn = min(minn, f[i - j * j]);
+            }
+            f[i] = minn + 1;
+        }
+        return f[n];
+    }
+};
