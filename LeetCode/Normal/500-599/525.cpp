6cbe2008848fb2b54492d9e428bd2b2fd4799ed4
ChenJG <dlvguo@qq.com>
2021-07-05 09:58:49 +0800

    up

---
+++ LeetCode/Normal/500-599/525.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int findMaxLength(vector<int> &nums)
+    {
+        int maxLength = 0;
+        unordered_map<int, int> mp;
+        int counter = 0;
+        mp[counter] = -1;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            int num = nums[i];
+            if (num == 1)
+            {
+                counter++;
+            }
+            else
+            {
+                counter--;
+            }
+            if (mp.count(counter))
+            {
+                int prevIndex = mp[counter];
+                maxLength = max(maxLength, i - prevIndex);
+            }
+            else
+            {
+                mp[counter] = i;
+            }
+        }
+        return maxLength;
+    }
+};
