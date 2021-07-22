2d44a4660d86e68f7392fa29ff178d56d63e4755
ChenJG <dlvguo@qq.com>
2021-07-22 20:50:05 +0800

    Create 138.cpp

---
+++ LeetCode/Normal/100-199/138.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *random;
+
+    Node(int _val)
+    {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+
+class Solution
+{
+public:
+    Node *copyRandomList(Node *head)
+    {
+        unordered_set<Node *> u_set;
+    }
+};
