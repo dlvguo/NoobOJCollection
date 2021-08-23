cbc22ad0e5f92776520b5fba7b8ab39d79f03526
ChenJG <dlvguo@qq.com>
2021-08-23 16:40:43 +0800

    up

---
+++ LeetCode/Normal/1700-1799/1713.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int minOperations(vector<int> &target, vector<int> &arr)
+    {
+    }
+};
---
+++ LeetCode/Normal/500-599/541.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int n = s.length();
+        for (int i = 0; i < n; i += 2 * k) {
+            reverse(s.begin() + i, s.begin() + min(i + k, n));
+        }
+        return s;
+    }
+};
+
+
