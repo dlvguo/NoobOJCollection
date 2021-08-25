25bc84a9e3bba4ab651884cea7d66511f980b80b
ChenJG <dlvguo@qq.com>
2021-08-25 12:31:16 +0800

    Create 797.cpp

---
+++ LeetCode/Normal/700-799/797.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int vis[16] = {0};
+    vector<vector<int>> vts;
+    int n;
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>> &graph)
+    {
+        vector<int> p = {0};
+        n = graph.size();
+        return vts;
+    }
+
+    void dfs(vector<int> &p, int src, vector<vector<int>> &graph)
+    {
+        vis[src] = 1;
+        for (int i = 0; i < graph.size(); i++)
+        {
+            /* code */
+        }
+    }
+};
