1b745b786a740a9f2ada9b6826039e5b247859cf
ChenJG <dlvguo@qq.com>
2022-01-01 20:43:19 +0800

    up

---
+++ LeetCode/Normal/2000-2099/2022.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<vector<int>> construct2DArray(vector<int> &original, int m, int n)
+    {
+        vector<vector<int>> ans;
+        if (original.size() != m * n)
+        {
+            return ans;
+        }
+        for (auto it = original.begin(); it != original.end(); it += n)
+        {
+            ans.emplace_back(it, it + n);
+        }
+        return ans;
+    }
+};
