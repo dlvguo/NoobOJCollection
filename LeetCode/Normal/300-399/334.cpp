5a09fc4b03bcd040b8c659ad98fce4e4b762d9f1
ChenJG <dlvguo@qq.com>
2022-01-12 19:44:20 +0800

    Create 334.cpp

---
+++ LeetCode/Normal/300-399/334.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool increasingTriplet(vector<int> &nums)
+    {
+        int n = nums.size();
+        if (n < 3)
+        {
+            return false;
+        }
+        vector<int> leftMin(n);
+        leftMin[0] = nums[0];
+        for (int i = 1; i < n; i++)
+        {
+            leftMin[i] = min(leftMin[i - 1], nums[i]);
+        }
+        vector<int> rightMax(n);
+        rightMax[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--)
+        {
+            rightMax[i] = max(rightMax[i + 1], nums[i]);
+        }
+        for (int i = 1; i < n - 1; i++)
+        {
+            if (nums[i] > leftMin[i - 1] && nums[i] < rightMax[i + 1])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
