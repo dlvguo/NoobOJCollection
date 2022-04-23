e6b99082aa3e6e91c17facfa7c8589de2c4adebe
ChenJG <dlvguo@qq.com>
2022-04-23 22:39:50 +0800

    Update 587.cpp

--- LeetCode/Normal/500-599/587.cpp
+++ LeetCode/Normal/500-599/587.cpp
@@ -1,30 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    int cross(const vector<int> & p, const vector<int> & q, const vector<int> & r) {
+    int cross(const vector<int> &p, const vector<int> &q, const vector<int> &r)
+    {
         return (q[0] - p[0]) * (r[1] - q[1]) - (q[1] - p[1]) * (r[0] - q[0]);
     }
 
-    vector<vector<int>> outerTrees(vector<vector<int>>& trees) {
+    vector<vector<int>> outerTrees(vector<vector<int>> &trees)
+    {
         int n = trees.size();
-        if (n < 4) {
+        if (n < 4)
+        {
             return trees;
         }
         /* 按照 x 大小进行排序，如果 x 相同，则按照 y 的大小进行排序 */
-        sort(trees.begin(), trees.end(), [](const vector<int> & a, const vector<int> & b) {
+        sort(trees.begin(), trees.end(), [](const vector<int> &a, const vector<int> &b)
+             {
             if (a[0] == b[0]) {
                 return a[1] < b[1];
             }
-            return a[0] < b[0];
-        });
+            return a[0] < b[0]; });
         vector<int> hull;
         vector<bool> used(n, false);
         /* hull[0] 需要入栈两次，不进行标记 */
         hull.emplace_back(0);
         /* 求出凸包的下半部分 */
-        for (int i = 1; i < n; i++) {
-            while (hull.size() > 1 && cross(trees[hull[hull.size() - 2]], trees[hull.back()], trees[i]) < 0) {
+        for (int i = 1; i < n; i++)
+        {
+            while (hull.size() > 1 && cross(trees[hull[hull.size() - 2]], trees[hull.back()], trees[i]) < 0)
+            {
                 used[hull.back()] = false;
                 hull.pop_back();
             }
@@ -33,9 +39,12 @@ public:
         }
         int m = hull.size();
         /* 求出凸包的上半部分 */
-        for (int i = n - 2; i >= 0; i--) {
-            if (!used[i]) {
-                while (hull.size() > m && cross(trees[hull[hull.size() - 2]], trees[hull.back()], trees[i]) < 0) {
+        for (int i = n - 2; i >= 0; i--)
+        {
+            if (!used[i])
+            {
+                while (hull.size() > m && cross(trees[hull[hull.size() - 2]], trees[hull.back()], trees[i]) < 0)
+                {
                     used[hull.back()] = false;
                     hull.pop_back();
                 }
@@ -46,7 +55,8 @@ public:
         /* hull[0] 同时参与凸包的上半部分检测，因此需去掉重复的 hull[0] */
         hull.pop_back();
         vector<vector<int>> res;
-        for(auto & v : hull) {
+        for (auto &v : hull)
+        {
             res.emplace_back(trees[v]);
         }
         return res;
